4e31363f02b9dec038460b56c017008c4321d90a
feernandobraga <fernando.bragga@gmail.com>
2019-08-13 21:19:10 +1000

    Exercise One included

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Some more simple exercises coded on week 3
   Write a Java program that determines if a given String is a palindrome. 
   Your program should be able to use different Strings by easily modifying your code.
  
+2. Exercise 1
+   * Write a program to take an input number from user between 1 and 24. Use if statement and case statement to display the output. 
+   If the input is less than 18, print a "Good day" greeting, otherwise print "Good evening".
+   Write a while loop that prints out multiples of 5 between 10 and 95
+   Write a while loop to add up the values 1 to 10 and print the sum once the loop has finished
--- src/ExerciseOne.java
+++ src/ExerciseOne.java
@@ -1,17 +1,12 @@
-//Write a program to take an input number from user between 1 and 24. Use if
-//        statement and case statement to display the output. If the input is less than 18, print
-//        a "Good day" greeting, otherwise print "Good evening".
-//        • Write a while loop that prints out multiples of 5 between 10 and 95
-//        • Write a while loop to add up the values 1 to 10 and print the sum once the loop has
-//        finished
-
 import java.util.Scanner;
 public class ExerciseOne {
 
     public static void main(String[] args) {
         Scanner userIO = new Scanner(System.in);
         int numberEntered = 0;
-        int fiveToNineFive = 5;
+        int tenToNineFive = 10;
+        int oneToTen = 1;
+        int sum = 0;
 
         // Asks the user to enter a number between 1 and 24
         // only breaks the loop if the conditions are met
@@ -28,12 +23,19 @@ public class ExerciseOne {
             System.out.println("Good evening ^_^");
         }
 
-        while (fiveToNineFive <= 95) {
-            if (fiveToNineFive % 5 == 0) {
-                System.out.println(fiveToNineFive + " is a multiple of five");
+        // Write a while loop that prints out multiples of 5 between 10 and 95
+        while (tenToNineFive <= 95) {
+            if (tenToNineFive % 5 == 0) {
+                System.out.println(tenToNineFive + " is a multiple of five");
             }
-            fiveToNineFive++;
+            tenToNineFive++;
         }
 
+        // Write a while loop to add up the values 1 to 10 and print the sum once the loop has finished
+        while (oneToTen <= 10) {
+            sum = sum + oneToTen;
+            oneToTen++;
+        }
+        System.out.println("\nThe sum is: " + sum);
     }
 }
